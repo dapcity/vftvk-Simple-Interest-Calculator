414a927e7eea2164a0158a112ad5541366542510
Dapo Ashaolu <dashaolu@gmail.com>
2021-04-11 06:34:03 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
+//Calculates the interest and outputs it to page
 {
-    p = document.getElementById("principal").value;
+    principle = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = principle * rate * years / 100
+    var currentYear = 2021;
+    
+    noOfYears = +currentYear + +years;
+
+    var result = document.getElementById('result');
+    result = "If you deposit " + principle + ", <br> at an interest rate of " + rate + "%. <br> You will receive an amount of " + interest + ", <br> in the year " + noOfYears
+
+    document.getElementById("result").innerHTML = result
+
     
 }
-        
+
+function validateForm(){
+//validates that there field is not blank or has a 0
+    principle = document.getElementById("principal").value;
+
+    if (principle == "" || principle == "0") {
+        alert("Enter numnber greater than 0");
+        return false;
+        }
+
+}
